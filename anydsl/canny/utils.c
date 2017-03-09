3ca6a4c8c1b0331f8fa13f735dc19115c74233eb
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2017-03-09 00:24:06 -0300

    Add canny sources
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

---
+++ anydsl/canny/utils.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Image allocation */
+float *allocate_image_data(int width, int height) {
+  if(width > 0 && height > 0) {
+    return (float *) malloc(width * height * sizeof(float));
+  }
+
+  return NULL;
+}
+
+/* Image deallocation */
+void free_image_data(float *data) {
+  free(data);
+}
+
+/* Synthetical image printing */
+void print_image_data(float *data, int width, int height) {
+  unsigned int i, j;
+
+  for(j = 0; j < height; ++j) {
+    for(i = 0; i < width; ++i) {
+      fprintf(stdout, "%.4f ", data[j * width + i]);
+    }
+
+    fprintf(stdout, "\n");;
+  }
+
+  fprintf(stdout, "\n");
+}
+
+/* Float sqrt */
+float float_sqrt(float value) {
+  return sqrtf(value);
+}

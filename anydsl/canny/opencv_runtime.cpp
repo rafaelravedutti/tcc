f2f78896f54cc760b4865cb8db5af55623d7a997
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2017-03-19 22:21:47 -0300

    Write opencv_canny function
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- anydsl/canny/opencv_runtime.cpp
+++ anydsl/canny/opencv_runtime.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 static cv::Mat img;
@@ -8,6 +9,7 @@ typedef int pixel_t;
 
 extern "C" {
   pixel_t *load_image(const char *path, int *width, int *height);
+  pixel_t *opencv_canny(unsigned char threshold, bool display);
   void display_image(const char *title);
   void release_images();
 }
@@ -21,13 +23,25 @@ pixel_t *load_image(const char *path, int *width, int *height) {
   return img.ptr<pixel_t>(0);
 }
 
+pixel_t *opencv_canny(unsigned char threshold, bool display) {
+  cv::Mat dest;
+
+  dest.create(img.size(), img.type());
+
+  cv::blur(img, dest, cv::Size(3,3));
+  cv::Canny(dest, dest, threshold, threshold * 3, 3);
+
+  if(display) {
+    cv::namedWindow("canny_result", cv::WINDOW_NORMAL);
+    cv::imshow("canny_result", dest);
+    cv::waitKey(0);
+  }
+
+  return dest.ptr<pixel_t>(0);
+}
+
 void display_image(const char *title) {
   cv::namedWindow(title, cv::WINDOW_NORMAL);
   cv::imshow(title, img);
   cv::waitKey(0);
-}
-
-
-void release_images() {
-
 }

0109d7ed15653e71fb4141717e257dd5f265eb44
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2017-06-14 22:40:37 -0300

    Commit changes to fix CUDA code
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

---
+++ anydsl/canny/changes.cu
@@ -0,0 +1,6 @@
+__device__ void write_39874(struct_14374 buf_39876, int i_39877, float v_39878) {
+    #line 14 "gpu_device.impala"
+    char* _39881;
+    _39881 = buf_39876.e1;
+    *((float *) _39881 + i_39877) = v_39878;
+}
